ac19e9427ebcc6fc89a3a4b8ef197e3c5827ff53
Arun samdoss <76962601+arunsamdoss@users.noreply.github.com>
2021-01-13 18:59:10 +0530

    Add files via upload

---
+++ src/views/secure.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="secure">
+        <h1>Welcome Home</h1>
+        <p>
+            user loged in 
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
